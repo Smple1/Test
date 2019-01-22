9e77b9496b1e9e31a5ebce5d830c6622f3b309da
Smple1 <253498923@qq.com>
2019-01-22 21:22:04 +0800

    test

---
+++ src/main/java/com/svzz/springbootssm/SpringbootssmApplication.java
@@ -0,0 +1,14 @@
+package com.svzz.springbootssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootssmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootssmApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/svzz/springbootssm/servlet.java
@@ -0,0 +1,7 @@
+package com.svzz.springbootssm;
+
+public class servlet {
+            public  void main(String adt[]){
+                System.out.print(1111);
+            }
+}
